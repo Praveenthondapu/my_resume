f8e8e99f1cbd8f179f1917641e14d4ecec8429de
Praveen Thondapu <praveenthondapu1996@gmail.com>
2020-01-13 11:23:57 +0530

    Updates

---
+++ precache-manifest.2f794b0011e4773b20a7e20d03cbeb49.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f130501681cb3d0129f266820c78b24",
+    "url": "/Praveenthondapu/my_resume.git/static/media/js.7f130501.png"
+  },
+  {
+    "revision": "80165b52741072e375ba",
+    "url": "/Praveenthondapu/my_resume.git/static/css/main.84e3e968.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Praveenthondapu/my_resume.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4bc1d229861ece2f79b8",
+    "url": "/Praveenthondapu/my_resume.git/static/js/1.4bc1d229.chunk.js"
+  },
+  {
+    "revision": "3488dbda7c8e1788dedb",
+    "url": "/Praveenthondapu/my_resume.git/static/js/runtime~main.3488dbda.js"
+  },
+  {
+    "revision": "16c41199eac9f9a92cc5184995dedf12",
+    "url": "/Praveenthondapu/my_resume.git/static/media/photo.16c41199.JPG"
+  },
+  {
+    "revision": "c5e995b6c047ca6ed3f323b3ad02e82b",
+    "url": "/Praveenthondapu/my_resume.git/static/media/HTML.c5e995b6.png"
+  },
+  {
+    "revision": "ca6798687edc23a2fbaefe15885f0ad0",
+    "url": "/Praveenthondapu/my_resume.git/static/media/java.ca679868.jpg"
+  },
+  {
+    "revision": "80165b52741072e375ba",
+    "url": "/Praveenthondapu/my_resume.git/static/js/main.80165b52.chunk.js"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/Praveenthondapu/my_resume.git/static/media/react.04557c1b.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Praveenthondapu/my_resume.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Praveenthondapu/my_resume.git/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Praveenthondapu/my_resume.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Praveenthondapu/my_resume.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "4bc1d229861ece2f79b8",
+    "url": "/Praveenthondapu/my_resume.git/static/css/1.9e1aa0dc.chunk.css"
+  },
+  {
+    "revision": "fe0ccd80eeb94b1ef1c72280de6c67f0",
+    "url": "/Praveenthondapu/my_resume.git/index.html"
+  }
+];
